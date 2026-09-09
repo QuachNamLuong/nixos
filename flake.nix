f01839db87c6e8e2802e83f3d19d11ae24cb2085
Quach Nam Luong <quachnamluong@gmail.com>
2026-09-09 11:56:01 +0700

    format flake.mix file

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -29,7 +29,18 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
   
-  outputs = inputs @ { self, nixpkgs, home-manager, noctalia, noctalia-greeter, umbriel, nix-flatpak, flake-utils, nix-vscode-extensions, ... }:
+  outputs = inputs @ { 
+    self, 
+    nixpkgs, 
+    home-manager, 
+    noctalia, 
+    noctalia-greeter, 
+    umbriel, 
+    nix-flatpak, 
+    flake-utils, 
+    nix-vscode-extensions, 
+    ...
+    }:
     let
       stateVersion = "26.05";
       username = "luong";
